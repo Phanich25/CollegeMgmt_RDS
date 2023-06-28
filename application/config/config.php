d0b000afd2d1b2d25031bc90c6a505e42d2dabb0
Phanich25 <76997439+Phanich25@users.noreply.github.com>
2023-06-28 18:17:33 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://phaniapp.azurewebsites.net';
+$config['base_url'] = 'http://3.110.29.196/CollegeMgmt_RDS/';
 
 /*
 |--------------------------------------------------------------------------
